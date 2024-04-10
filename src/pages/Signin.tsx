24955a28c3232352123b289eed13534b75c18e7a
rakeshpaulraj <rakeshpaulraj2004@gmail.com>
2024-04-11 00:45:39 +0530

    first page finished

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -1,15 +1,22 @@
 import { SigninCard } from "../components/SigninCard"
-import { Quote } from "../components/Quote"
+import { TextGenerateEffect } from "../components/Textgenerator"
+
 
 export const Signin = () => {
-    return <div>
-        <div className="grid grid-cols-1 lg:grid-cols-2">
+    return <div className="h-full w-full flex md:items-center md:justify-center bg-black/[0.98] antialiased bg-grid-white/[0.05] relative overflow-hidden">
+        <div className="p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0 grid grid-cols-1 md:grid-cols-2">
+        <div className=" p-5 flex justify-center items-center">
             <div>
-               <SigninCard/>
-            </div>
-            <div className="hidden lg:block">
-                <Quote />
+            <div className="text-6xl lg:text-8xl animate-gradient to-65% bg-gradient-to-br bg-clip-text bg-right-bottom font-extrabold text-transparent from-white from-35% via-[#3178c6] to-[#3178c6] bg-[length:300%_300%]">
+                    Writeit
+                </div>
+                <div>
+                    <TextGenerateEffect words="Created by Rakesh.P" />
+                </div>
             </div>
         </div>
+        <SigninCard />
+    </div>
+        
     </div>
 }
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,15 +1,20 @@
 import { SignupCard } from "../components/SignupCard"
-import { Quote } from "../components/Quote"
+import { TextGenerateEffect } from "../components/Textgenerator"
 
 export const Signup = () => {
-    return <div >
-        <div className="grid grid-cols-1 lg:grid-cols-2" >
+    return <div className="h-full w-full flex md:items-center md:justify-center bg-black/[0.98] antialiased bg-grid-white/[0.05] relative overflow-hidden">
+        <div className="p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0 grid grid-cols-1 md:grid-cols-2">
+        <div className=" p-5 flex justify-center items-center">
             <div>
-            <SignupCard/>
-            </div>
-            <div className="hidden lg:block">
-                <Quote />
+            <div className="text-6xl lg:text-8xl animate-gradient to-65% bg-gradient-to-br bg-clip-text bg-right-bottom font-extrabold text-transparent from-white from-35% via-[#3178c6] to-[#3178c6] bg-[length:300%_300%]">
+                    Writeit
+                </div>
+                <div>
+                    <TextGenerateEffect words="Created by Rakesh.P" />
+                </div>
             </div>
         </div>
+        <SignupCard />
     </div>
-}
+        
+    </div>}
